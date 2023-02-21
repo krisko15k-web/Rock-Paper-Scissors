5ffba65f6a53a94c6a20fc0e09f15d11e987512a
krisko15k-web <81244898+krisko15k-web@users.noreply.github.com>
2023-02-21 22:40:53 +0200

    Creating Files

---
+++ script.js
@@ -0,0 +1,47 @@
+let playerScore = 0
+let computerScore = 0
+
+function getRandChoice() {
+    let randChoice = ['Rock', 'Paper', 'Scissors']
+    return randChoice[Math.floor(Math.random() * randChoice.length)]
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return `Draw! ${playerSelection} = ${computerSelection}`
+    } else if (playerSelection == 'Rock' && computerSelection == 'Paper') {
+        computerScore++
+        return 'You Lose! Paper beats Rock'
+    } else if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
+        playerScore++
+        return 'You Win! Rock beats Scissors'
+    } else if (playerSelection == 'Paper' && computerSelection == 'Scissors') {
+        computerScore++
+        return 'You Lose! Scissors beats Paper'
+    } else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
+        playerScore++
+        return 'You Win! Paper beats Rock'
+    } else if (playerSelection == 'Scissors' && computerSelection == 'Rock') {
+        computerScore++
+        return 'You Lose! Rock beats Scissors'
+    } else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
+        playerScore++
+        return 'You Win! Scissors beats Paper'
+    }
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        console.log(playRound(getRandChoice(), getRandChoice()))
+    }
+    console.log(`Player score: ${playerScore}`)
+    console.log(`Computer score: ${computerScore}`)
+    if (playerScore > computerScore) {
+        console.log('Player Wins')
+    } else if (playerScore < computerScore) {
+        console.log('Computer Wins')
+    } else {
+        console.log('Draw')
+    }
+}
+game()
